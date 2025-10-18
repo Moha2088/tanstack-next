c7f588a61586fc20cfd68b9b6c228fdf7e87031b
Mohamed Shil <maxamed-14@outlook.dk>
2025-10-18 16:10:49 +0200

    add mutation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,95 @@
-"use client"
+"use client";
 
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
-import { useState } from "react"
-import { Todo } from "@/app/types/types"
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { Todo } from "@/app/types/types";
+import axios from "axios";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function Home() {
-    const queryClient = new QueryClient()
+  const [fetchData, setFetchData] = useState<boolean>(false);
+  const [postData, setPostData] = useState<boolean>(false);
 
-    const [isButtonClicked, setIsButtonClicked] = useState<boolean>(false)
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="flex justify-center items-center mt-10 mb-10 flex-col">
+        <div className="mb-10">
+          <button
+            className=" rounded-md p-3 bg-black text-white font-bold cursor-pointer"
+            onClick={() => setFetchData(true)}
+          >
+            Fetch Data
+          </button>
+        </div>
+        <div className="flex justify-center">{fetchData && <FetchData />}</div>
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <div className="flex justify-center mt-10 mb-10">
-                <button
-                    className=" rounded-md p-3 bg-black text-white font-bold cursor-pointer"
-                    onClick={() => setIsButtonClicked(true)}>Fetch Data
-                </button>
-            </div>
-            <div className="flex justify-center">
-                {isButtonClicked &&
+        <div>
+          <button
+            className=" rounded-md p-3 bg-black text-white font-bold cursor-pointer"
+            onClick={() => setPostData(true)}
+          >
+            Post Data
+          </button>
+          <div>{postData && <p>POST request success!</p>}</div>
+        </div>
+      </div>
+    </QueryClientProvider>
+  );
+}
 
-                    <FetchData/>
-                }
-            </div>
-        </QueryClientProvider>
-    );
+function PostData() {
+  const mutation = useMutation({
+    mutationFn: async (newTodo: Todo) => {
+      await axios.post("https://jsonplaceholder.typicode.com/todos", newTodo);
+    },
+    onSuccess: () => {
+      console.log("Data posted successfully");
+    },
+  });
+
+  useEffect(() => {
+    mutation.mutate({
+      id: 300,
+      userId: 400,
+      title: "New Todo Item",
+      completed: false,
+    });
+  }, [mutation]);
 }
 
 function FetchData() {
-    const {isPending, error, data} = useQuery({
-        queryKey: ['todoData'],
-        queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos/2')
-            .then(res => res.json())
+  const { isPending, error, data } = useQuery({
+    queryKey: ["todoData"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos/2").then((res) =>
+        res.json()
+      ),
+  });
 
-    })
+  if (isPending) return <p>Loading</p>;
 
-    if (isPending) return <p>Loading</p>
+  if (error) return <p>Error fetching data!: {error.message}</p>;
 
-    if (error) return <p>Error fetching data!: {error.message}</p>
-
-    return (
-        <div>
-            <h1><strong>Title: </strong> {data.title} </h1>
-            <p><strong>Id: </strong> {data.id} </p>
-            <p><strong>UserId: </strong> {data.userId} </p>
-            <p><strong>IsCompleted: </strong> {data.completed} </p>
-        </div>
-
-    )
-
+  return (
+    <div>
+      <h1>
+        <strong>Title: </strong> {data.title}{" "}
+      </h1>
+      <p>
+        <strong>Id: </strong> {data.id}{" "}
+      </p>
+      <p>
+        <strong>UserId: </strong> {data.userId}{" "}
+      </p>
+      <p>
+        <strong>IsCompleted: </strong> {data.completed}{" "}
+      </p>
+    </div>
+  );
 }
--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,6 +1,6 @@
 export interface Todo {
     userId: number;
-    id: string;
+    id: number;
     title: string;
     completed: boolean;
 }
